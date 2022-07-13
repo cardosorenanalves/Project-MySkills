4def0188d1c09bb3fae97d85f21e04f3ad8186e0
cardosorenanalves <renancardoso0307@gmail.com>
2022-07-13 09:38:49 -0300

    Projeto Myskills iniciado

---
+++ src/components/Button.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import {
+    TouchableOpacity, 
+    TouchableOpacityProps,
+    Text, 
+    StyleSheet
+} from 'react-native'
+
+interface ButtonProps extends TouchableOpacityProps{
+    title: string;
+}
+
+export function Button({ title, ...rest } : ButtonProps){
+    return(
+        <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={.7}
+        {...rest}
+        >
+        <Text style={styles.buttonText}>{title}</Text>
+      </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    button: {
+      backgroundColor: '#a370f7',
+      padding: 15,
+      borderRadius: 7,
+      display: "flex",
+      alignItems: "center",
+      marginTop: 20,
+
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontSize: 17,
+        fontWeight: "bold",
+    },
+})
---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+    TouchableOpacity,
+    Text,
+    StyleSheet,
+    TouchableOpacityProps
+} from 'react-native';
+
+interface SkillCardProps extends TouchableOpacityProps{
+  skill: string;
+}
+
+export function SkillCard({skill, ...rest } : SkillCardProps){
+    return(
+        <TouchableOpacity 
+        style={styles.buttonSkill}
+        {...rest}
+        >
+
+        <Text style={styles.textSkill}>
+          {skill}
+        </Text>
+        
+      </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    textSkill: {
+        color: '#fff',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+
+    buttonSkill: {
+        backgroundColor: '#1f1e25',
+        padding: 15,   
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 10,
+      
+      }
+
+})
